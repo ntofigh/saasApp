5008b3df15363cb1b712ef966b284e10510a9f0e
Nora Tofigh <no.tofigh@gmail.com>
2015-12-18 16:48:30 +0000

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < Application::Base
+   default to: 'no.tofigh@gmail.com'
+   
+    def contact_email(name, email, body)
+     @name = name
+     @email = email
+     @body = body
+     
+     mail(from: email, subject: 'Contact Form Message')
+    end
+end
