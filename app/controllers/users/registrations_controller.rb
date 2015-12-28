14d5f8d35e78821c09e81dcfe27c0dabb9e1d6ff
Nora Tofigh <no.tofigh@gmail.com>
2015-12-28 01:54:43 +0000

    made change of spacing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,8 +14,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
   
-
-
 private
   def select_plan
     unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
